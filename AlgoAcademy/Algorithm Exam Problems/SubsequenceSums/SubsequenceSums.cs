ccb9fdc7f2c4ddf8fc9012d3ee359042ca827769
ROSSFilipov <ruslanfilipov@yahoo.com>
2015-12-25 21:26:24 +0200

    Added new project

---
+++ AlgoAcademy/Algorithm Exam Problems/SubsequenceSums/SubsequenceSums.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SubsequenceSums
+{
+    private static long[,] binomMatrix = new long[1000, 1000];
+    static void Main(string[] args)
+    {
+        ExecuteSolution();
+    }
+
+    public static long CalculateSum(int[] numbers)
+    {
+        long sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static void ExecuteSolution()
+    {
+        int turns = int.Parse(Console.ReadLine());
+        for (int i = 0; i < turns; i++)
+        {
+            string[] nAndKValues = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            short n = short.Parse(nAndKValues[0]);
+            short k = short.Parse(nAndKValues[1]);
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            Console.WriteLine(CalculateCurrentSum(n, k, numbers));
+        }
+    }
+
+    public static long CalculateCurrentSum(int n, int k, int[] numbers)
+    {
+        long binomCoefficient = CalculateBinomCoefficient(n - 1, k);
+        long currentSum = binomCoefficient * CalculateSum(numbers);
+        return currentSum;
+    }
+
+    private static long CalculateBinomCoefficient(int n, int k)
+    {
+        if (k > n)
+        {
+            return 0;
+        }
+        if (k == 0 || k == n)
+        {
+            return 1;
+        }
+        if (binomMatrix[n, k] == 0)
+        {
+            binomMatrix[n, k] = CalculateBinomCoefficient(n - 1, k - 1) + CalculateBinomCoefficient(n - 1, k);
+        }
+
+        return binomMatrix[n, k];
+    }
+}
+
---
+++ AlgoAcademy/Algorithm Exam Problems/ZigZagSequences/ZigZagSequences.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ZigZagSequences
+{
+    private static int[] numbersArray;
+    private static int[] sequenceArray;
+    private static bool[] used;
+    private static int solutionsCounter = 0;
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int arraySize = int.Parse(input[0]);
+        int sequenceSize = int.Parse(input[1]);
+        numbersArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            numbersArray[i] = i;
+        }
+
+        sequenceArray = new int[sequenceSize];
+        used = new bool[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            //sequenceArray[0] = numbersArray[i];
+            used[i] = true;
+            CountZigZagSequences(1, sequenceSize, 1, numbersArray[i], arraySize);
+            used[i] = false;
+        }
+
+        Console.WriteLine(solutionsCounter);
+    }
+
+    private static void CountZigZagSequences(int index, int sequenceSize, int zigZagIndex, int prevNumber, int arraySize)
+    {
+        if (index >= sequenceSize)
+        {
+            solutionsCounter++;
+            //Console.WriteLine(string.Join(", ", sequenceArray));
+            return;
+        }
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            if (zigZagIndex == 1 && numbersArray[i] < prevNumber && !used[i])
+            {
+                used[i] = true;
+                //sequenceArray[index] = numbersArray[i];
+                CountZigZagSequences(index + 1, sequenceSize, 0, numbersArray[i], arraySize);
+                used[i] = false;
+            }
+            else if (zigZagIndex == 0 && numbersArray[i] > prevNumber && !used[i])
+            {
+                used[i] = true;
+                //sequenceArray[index] = numbersArray[i];
+                CountZigZagSequences(index + 1, sequenceSize, 1, numbersArray[i], arraySize);
+                used[i] = false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
+
